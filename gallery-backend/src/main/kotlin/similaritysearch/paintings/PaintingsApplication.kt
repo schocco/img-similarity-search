a595bc77b618b104ebe60e118a8610a748769e8a
Rocco Schulz <schocco.rulz@gmail.com>
2020-01-07 10:54:53 +0100

    bootstrap spring boot app as a backend for the UI

---
+++ gallery-backend/src/main/kotlin/similaritysearch/paintings/PaintingsApplication.kt
@@ -0,0 +1,11 @@
+package similaritysearch.paintings
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaintingsApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaintingsApplication>(*args)
+}
---
+++ gallery-backend/src/test/kotlin/similaritysearch/paintings/PaintingsApplicationTests.kt
@@ -0,0 +1,13 @@
+package similaritysearch.paintings
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaintingsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
